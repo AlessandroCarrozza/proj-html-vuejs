2eaa1d371035082045ffe90030b1e136de7ac4c9
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-04-11 16:46:37 +0200

    Lavorazione header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,105 @@
+<script>
+export default{
+    name: "AppHeader",
+    data() {
+        return{
+            navbarList: [],
+        }
+    }
+}
+</script>
+
+
+<template>
+    <header>
+        <div class="header-top">
+            <div class="container">
+                <div class="logo">
+                    <img src="../../public/dark-pet-logo.png" alt="Logo">
+                </div>
+                <div>
+                    <div class="search">
+                        <span><i class="fa-solid fa-magnifying-glass"></i></span>
+                        <input type="text" placeholder="Search...">
+                    </div>
+                </div>
+                <div class="call">
+                    <h4>Questions?</h4>
+                    <span>Call us 33932939293932</span>
+                </div>
+                <div class="icons">
+                    <i class="fa-solid fa-store"></i>
+                    <i class="fa-regular fa-user"></i>
+                </div>
+            </div>
+        </div>
+
+        <div class="header-bottom"></div>
+    </header>
+</template>
+
+
+<style scoped lang="scss">
+header{
+    height: 150px;
+    .header-top, .header-bottom{
+        height: 50%;
+    }
+    .header-top{
+        border-bottom: 1px solid lightgrey;
+        display: flex;
+        justify-content: space-between;
+
+        .container{
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+
+        .logo img{
+            height: 40px;
+        }
+
+        .search{
+            width: 250px;
+            height: 40px;
+            display: flex;
+
+            span{
+                width: 30px;
+                background-color: lightgrey;
+                display: inline-block;
+                border-top-left-radius: 50px;
+                border-bottom-left-radius: 50px;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                padding-left: 0.6rem;
+
+                i{
+                    font-size: 0.8rem;
+                }
+            }
+
+            input{
+                width: 220px;
+                border: none;
+                background-color: lightgrey;
+                border-top-right-radius: 50px;
+                border-bottom-right-radius: 50px;
+                padding-left: 0.6rem;
+            }
+        }
+
+        .call h4{
+            display: inline-block;
+        }
+
+        .icons i{
+            padding: 0 0.6rem;
+            font-size: 1.3rem;
+            cursor: pointer;
+        }
+    }
+}
+</style>
